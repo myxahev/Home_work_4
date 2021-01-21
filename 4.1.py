7d4ac08c69d46f674afe9e9ec6b9b1ceb07ed569
Dmitry <myxahev2@gmail.com>
2021-01-22 00:44:59 +0500

    update 4.1

--- 4.1.py
+++ 4.1.py
@@ -1,14 +1,15 @@
+# Реализовать скрипт, в котором должна быть предусмотрена функция расчёта заработной платы сотрудника.
+# Используйте в нём формулу: (выработка в часах*ставка в час) + премия.
+# Во время выполнения расчёта для конкретных значений необходимо запускать скрипт с параметрами.
+
+
 from sys import argv
 
 script_name, first_param, second_param, third_param = argv
 
 def simple_calc():
-    x = int(first_param)
-    y = int(second_param)
-    z = int(third_param)
-    return (x * y) + (z * (x * y))
-
-
+    x, y, z = int(first_param), int(second_param), int(third_param)
+    return x * y + z
 
 print(f'Размер заработной платы составил: {simple_calc() } $')
 
