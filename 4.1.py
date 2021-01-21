045d667437a4a70617ca832e56a982e1f441b80b
Dmitry <myxahev2@gmail.com>
2021-01-22 00:04:36 +0500

    update 4.1

--- 4.1.py
+++ 4.1.py
@@ -5,13 +5,11 @@ script_name, first_param, second_param, third_param = argv
 def simple_calc():
     x = int(first_param)
     y = int(second_param)
-    z = third_param
-    if z == '30%':
-        return (x * y) + (0.3 * (x * y))
-    elif z == '50%':
-        return (x * y) + (0.5 * (x * y))
-    else:
-        return x * y
+    z = int(third_param)
+    return (x * y) + (z * (x * y))
+
+
+
 print(f'Размер заработной платы составил: {simple_calc() } $')
 
 # python 4.1.py 40 150 30%
