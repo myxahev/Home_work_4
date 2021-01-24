2ff8e14c84cc649caaf5e1c2f9846e4defca3e81
Dmitry <myxahev2@gmail.com>
2021-01-24 18:56:02 +0500

    update 4.2

--- 4.2.py
+++ 4.2.py
@@ -4,6 +4,5 @@
 # Результат: [12, 44, 4, 10, 78, 123].
 
 in_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-res_list = [number for i, number in enumerate(in_list) if i > 0 and in_list[i] > in_list[i - 1]]
-print(res_list)
+print([number for i, number in enumerate(in_list) if i > 0 and in_list[i] > in_list[i - 1]])
 
