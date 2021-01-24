05e8536bd141adfbdbdd205d5bbf7bf98f6a1fee
Dmitry <myxahev2@gmail.com>
2021-01-24 18:55:31 +0500

    update 4.3

--- 4.3.py
+++ 4.3.py
@@ -1,5 +1,4 @@
 # Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Решите задание в одну строку.
 # Подсказка: используйте функцию range() и генератор.
 
-list = [number for i, number in enumerate(range(20, 241)) if number % 20 == 0 or number % 21 == 0]
-print(list)
+print([number for i, number in enumerate(range(20, 241)) if number % 20 == 0 or number % 21 == 0])
